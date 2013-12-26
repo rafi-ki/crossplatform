4fc93bed9110ddabbb66cf2c3d0d484216388d26
rafikime <rafael.teyral@gmail.com>
2013-12-26 14:40:02 +0100

    erste seite mit localStorage funktioniert

---
+++ crossplatform.js
@@ -0,0 +1,29 @@
+function getName()
+{
+	var name = localStorage.name;
+	var result = "Hello, ";
+	if (name)
+		result = result + name;
+	else
+		result = result + "You";
+		
+	var par = document.getElementById("greetingParagraph");
+	par.innerHTML=result;
+}
+
+function saveName()
+{
+	var name = document.getElementById("name").value;
+	var errorPar = document.getElementById("errorMsg");
+	if (name)
+	{
+		localStorage.name = name;
+		errorPar.style.display="none";
+		getName();
+	}
+	else
+	{
+		errorPar.innerHTML="invalid name";
+		errorPar.style.display="block";
+	}	
+}
