11acc710e44d454b4ff35bea17adfb443ef180d1
rafikime <rafael.teyral@gmail.com>
2013-12-30 22:56:39 +0100

    added ajax request for loading listitems

--- crossplatform.js
+++ crossplatform.js
@@ -27,3 +27,56 @@ function saveName()
 		errorPar.style.display="block";
 	}	
 }
+
+function searchMovie()
+{
+	var name = document.getElementById("moviename").value;
+	document.getElementById("movietitle").innerHTML = name;
+	var data_file = "http://mymovieapi.com/?title=" + name + "&type=json&plot=simple&episode=1&limit=1&yg=0&mt=none&lang=en-US";
+   var http_request = new XMLHttpRequest();
+   try{
+      // Opera 8.0+, Firefox, Chrome, Safari
+      http_request = new XMLHttpRequest();
+   }catch (e){
+      // Internet Explorer Browsers
+      try{
+         http_request = new ActiveXObject("Msxml2.XMLHTTP");
+      }catch (e) {
+         try{
+            http_request = new ActiveXObject("Microsoft.XMLHTTP");
+         }catch (e){
+            // Something went wrong
+            alert("Your browser broke!");
+            return false;
+         }
+      }
+   }
+   http_request.onreadystatechange  = function(){
+      if (http_request.readyState == 4  )
+      {
+        // Javascript function JSON.parse to parse JSON data
+        var jsonObj = JSON.parse(http_request.responseText)[0];
+
+		document.getElementById("movietitle").innerHTML = jsonObj.title;
+		addListitems(jsonObj.actors);
+        // jsonObj variable now contains the data structure and can
+        // be accessed as jsonObj.name and jsonObj.country.
+        //document.getElementById("Name").innerHTML =  jsonObj.name;
+        //document.getElementById("Country").innerHTML = jsonObj.country;
+      }
+   }
+   http_request.open("GET", data_file, true);
+   http_request.send();
+}
+
+function addListitems(actors)
+{
+	var list = document.getElementById("actorslist");
+	var out = document.getElementById("output");
+	for (i=0; i<actors.length; i++)
+	{
+		var actor = document.createElement('li');
+		actor.appendChild(document.createTextNode(actors[i]));
+		list.appendChild(actor);
+	}
+}
